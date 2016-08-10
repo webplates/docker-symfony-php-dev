02f02423ddd28ddf0b5a94a37668cbebc67fb140
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2016-08-10 23:57:36 +0200

    Remove xdebug variant: choose debug tool locally

--- build.py
+++ build.py
@@ -9,11 +9,9 @@ REPO = "webplates/symfony-php-dev"
 
 VERSIONS = ["5.5.38", "5.6.24", "7.0.9"]
 VARIANTS = ["fpm"]
-SUBVARIANTS = ["xdebug"]
 
 MATRIX = set(itertools.chain(
-    itertools.product(VERSIONS, VARIANTS, [None]),
-    itertools.product(VERSIONS, VARIANTS, SUBVARIANTS)
+    itertools.product(VERSIONS, VARIANTS, [None])
 ))
 
 # Prepare Jinja
@@ -34,9 +32,8 @@ template = env.get_template('Dockerfile.template')
 for image in MATRIX:
     path = DIST + "/" + matrix_join((minorize(image[0]),) + image[1:], "/")
     os.makedirs(path, exist_ok=True)
-    dockerfile = path + "/Dockerfile"
     php_version = semver.parse(image[0])
-    template.stream(parent=matrix_join(image[0:2], "-"), php_version="%d%d" % (php_version["major"], php_version["minor"]), subvariant=image[2]).dump(dockerfile)
+    template.stream(parent=matrix_join(image[0:2], "-"), php_version="%d%d" % (php_version["major"], php_version["minor"])).dump(path + "/Dockerfile")
     paths.append(path)
     tags.append(set(get_tags(image)))
 
